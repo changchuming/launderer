4f8882d73878ffc979c745e643641877c81cfa30
changchuming <changchuming@gmail.com>
2016-10-01 23:52:34 +0800

    Removed redundant files

--- server/app.js
+++ server/app.js
@@ -20,18 +20,9 @@ var path = require('path');
 var util = require("util");
 
 //----------------------------------------------------------------------------------------------
-// Queue
-//----------------------------------------------------------------------------------------------
-var queue = require('./modules/queue');
-queue.initialize();
-queue.process();
-
-//----------------------------------------------------------------------------------------------
 // Routes
 //----------------------------------------------------------------------------------------------
 var index = require('./routes');
-var result = require('./routes/result');
-var drone = require('./routes/drone');
 
 //----------------------------------------------------------------------------------------------
 // Express - All environments
@@ -56,14 +47,6 @@ if ('development' == app.get('env')) {
 }
 
 //----------------------------------------------------------------------------------------------
-// Error handling if we are unable to connect to redis
-//----------------------------------------------------------------------------------------------
-
-redisClient.on("error", function (err) {
-	console.log("Redis server cannot be reached.");
-	});
-
-//----------------------------------------------------------------------------------------------
 // Create server and listen to port
 //----------------------------------------------------------------------------------------------
 
@@ -75,92 +58,12 @@ app.listen(app.get('port'), function(){
 // Socket routes
 //----------------------------------------------------------------------------------------------
 
-//Joins a room
-app.io.route('join', function(req) {
-    req.io.join(req.data);
-});
-
-// Leaves a room
-app.io.route('leave', function(req) {
-    req.io.leave(req.data);
-});
-
-// Special case when pi connects
-app.io.route('pijoin', function(req) {
-    drone.pijoin(req);
-});
-
-// Set new job with iid for pi
-app.io.route('pinewjob', function(req) {
-    drone.newjob(req);
-});
-
-// Asks a certain pi to take a picture
-app.io.route('pitakepic', function(req) {
-    drone.takepic(req);
-});
-
-// Asks a certain pi to add a picture
-app.io.route('piaddpic', function(req) {
-    drone.addpic(req);
-});
-
-// Remove picture from database
-app.io.route('piremovepic', function(req) {
-    drone.removepic(req);
-});
-
-// Remove picture from database
-app.io.route('piprocessjob', function(req) {
-    drone.processjob(req);
-});
-
-// Halt / shutdown a pi
-app.io.route('pihalt', function (req) {
-	drone.pihalt(req);
-});
-
-// Show connected pi's ip
-app.io.route('piip', function(req) {
-    console.log(req.data);
-});
-
-// Upload pics
-app.io.route('uploadpic', function(req) {
-    index.uploadpic(req);
-});
-
-//Remove picture from database
-app.io.route('removepic', function(req) {
-    index.removepic(req);
-});
-
-//Remove picture from database
-app.io.route('processjob', function(req) {
-    index.processjob(req);
-});
+// //Joins a room
+// app.io.route('join', function(req) {
+//     req.io.join(req.data);
+// });
 
 //##############################################################################################
 // Display landing page
 //##############################################################################################
 app.get('/', index.display);
-
-//##############################################################################################
-// Logic to upload photos
-//##############################################################################################
-app.post('/upload', index.upload);
-
-//##############################################################################################
-// Display pi landing page
-//##############################################################################################
-app.get('/drone', drone.display);
-
-//##############################################################################################
-//Render about page
-//##############################################################################################
-app.get('/about', index.about);
-
-//##############################################################################################
-// Display result of run
-//##############################################################################################
-app.get('/:jobid', result.display);
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,96 +6,16 @@
 //----------------------------------------------------------------------------------------------
 // Module dependencies
 //----------------------------------------------------------------------------------------------
-var app = require('../app');
-var async = require('async');
-var queue = require('../modules/queue');
-var jobs = app.jobs;
-var formidable = require("formidable");
-var util = require("util");
-var fs = require('fs');
  
 //##############################################################################################
 // Display home page
 //##############################################################################################
 exports.display = function(req, res){
   	// commenting out the main index page
-	var iid;
-	redisClient.get('lastiid', function(err, reply) {
-		if (reply == null) {
-			iid = 1;
-		} else {
-			iid = parseInt(reply)+1;
-		}
-		fs.mkdir('public/uploads/'+iid); // Make uploads folder for this job
-		redisClient.set('lastiid', iid, function (err, reply) {}); // Update last iid
-	  	res.render('index', {
-	  		title: 'Launderer',
-	  		iid: iid
-	  	});
-	});
+  	res.render('index', {
+  		title: 'Launderer',
+  	});
 };
-
-//##############################################################################################
-//Uploads files and adds to queue
-//##############################################################################################
-exports.upload = function(req, res){
-	var form = new formidable.IncomingForm(),
-            //files = [],
-            //fields = [],
-            //returnJson = {},
-            iid;
-	
-	form.keepExtensions = true;
-	form.multiples = true;
-	form.on('field', function(name, value) {
-		//fields.push([name, value]);
-		if (name='iid') {
-			form.uploadDir = "./public/uploads/"+value;
-			iid = value;
-		}
-	});
-    form.on ('fileBegin', function(name, file){
-			var fileType = file.type.split('/').pop();
-            if(fileType == 'jpg' || fileType == 'jpeg' ){
-                //rename the incoming file to the current timestamp
-            	file.path = form.uploadDir + "/" + Date.now() + ".jpg";
-            } else {
-                file.path = form.uploadDir + "/" + Date.now() + "." + fileType;
-            }
-    })
-    .on('file', function(name, file) {
-            //on file received
-            //files.push([name, file]);
-            var fileType = file.type.split('/').pop();
-            if(fileType == 'jpg' || fileType == 'jpeg' ){
-            	app.io.room('iid'+iid).broadcast('addpic', file.path.replace('public', ''));
-            }
-        })
-    
-    form.parse(req);
-    res.send('done');
-}
-
-//##############################################################################################
-// Client calls remove pic and server removes pic
-//##############################################################################################
-exports.removepic = function(req) {
-	try {
-		fs.unlinkSync('public/'+req.data);
-	} catch (ex) {
-		console.log(ex);
-	}
-}
-
-//##############################################################################################
-// Client calls for processing job using iid and server processes
-//##############################################################################################
-exports.processjob = function(req) {
-console.log(req.data);
-	queue.newJob(req.data, function(jobid) {
-		app.io.room('iid'+req.data).broadcast('setjobid', jobid);
-	});
-}
 
 //##############################################################################################
 //Display about page
